161b5cf7563a207598656f273a8e9ffa90ae67bf
Pierre-Francois Houessou <81161636+hpfpv@users.noreply.github.com>
2022-01-01 23:45:18 +0100

    Update get_stories_tagged.py

--- backend/instaScraper/get_stories_tagged.py
+++ backend/instaScraper/get_stories_tagged.py
@@ -51,52 +51,63 @@ def lambda_handler(event, context):
     account_to_mention = event['pathParameters']['account_to_mention']
     # logger.info("Account to mention", account_to_mention)
     stories = get_followers_stories_if_mentionned(account_to_mention)
-    response = []
 
-    logger.info(stories)
-    for story in stories:
-        story_owner = story["node"]["owner"]["username"]
-        story_id = story["node"]["id"]
-        story_owner_profile_pic_url = story["node"]["owner"]["profile_pic_url"]
-        story_is_video = story["node"]["is_video"]
-        taken_at_timestamp = story["node"]["taken_at_timestamp"]
-        time = story_time_str(taken_at_timestamp)
-        if story_is_video:
-            story_video_url = story["node"]["video_resources"][0]["src"]
-            story_media_url = story_video_url
-            story_display_url = story["node"]["display_url"]
-            story_duration = story["node"]["video_duration"] * 1000
-        else:
-            story_video_url = ""
-            story_display_url = story["node"]["display_url"]
-            story_media_url = story_display_url
-            story_duration = 5000
+    if(stories):
+        logger.info(stories)
+        response = []
+        for story in stories:
+            story_owner = story["node"]["owner"]["username"]
+            story_id = story["node"]["id"]
+            story_owner_profile_pic_url = story["node"]["owner"]["profile_pic_url"]
+            story_is_video = story["node"]["is_video"]
+            taken_at_timestamp = story["node"]["taken_at_timestamp"]
+            time = story_time_str(taken_at_timestamp)
+            if story_is_video:
+                story_video_url = story["node"]["video_resources"][0]["src"]
+                story_media_url = story_video_url
+                story_display_url = story["node"]["display_url"]
+                story_duration = story["node"]["video_duration"] * 1000
+            else:
+                story_video_url = ""
+                story_display_url = story["node"]["display_url"]
+                story_media_url = story_display_url
+                story_duration = 5000
 
-        profile_picture(story_owner_profile_pic_url, story_owner)
-        story_media(story_video_url, story_display_url, story_is_video, story_id)
+            profile_picture(story_owner_profile_pic_url, story_owner)
+            story_media(story_video_url, story_display_url, story_is_video, story_id)
 
-        data = {
-            'story_id': story_id,
-            'story_time': time,
-            'user': story_owner,
-            # "story_owner_profile_pic_url": story_owner_profile_pic_url,
-            # 'path_to_profile_pic': path_to_profile_pic,
-            'is_video': story_is_video,
-            "story_media_url": story_media_url,
-            'media': story_id,
-            'time': story_duration
+            data = {
+                'story_id': story_id,
+                'story_time': time,
+                'user': story_owner,
+                # "story_owner_profile_pic_url": story_owner_profile_pic_url,
+                # 'path_to_profile_pic': path_to_profile_pic,
+                'is_video': story_is_video,
+                "story_media_url": story_media_url,
+                'media': story_id,
+                'time': story_duration
+            }
+
+            response.append(data)
+        logger.info(response)
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': 'https://instastories.houessou.com',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'GET',
+                'Content-Type': 'application/json'
+            },
+            'body': json.dumps(response)
         }
-
-        response.append(data)
-    logger.info(response)
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': 'https://instastories.houessou.com',
-            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-            'Access-Control-Allow-Methods': 'GET',
-            'Content-Type': 'application/json'
-        },
-        'body': json.dumps(response)
-    }
-
+    else:
+        return {
+            'statusCode': 204,
+            'headers': {
+                'Access-Control-Allow-Origin': 'https://instastories.houessou.com',
+                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+                'Access-Control-Allow-Methods': 'GET',
+                'Content-Type': 'application/json'
+            },
+            'body': ''
+        }
