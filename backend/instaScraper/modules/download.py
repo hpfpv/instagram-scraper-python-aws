bd850aecb802aaf09ceb3145eed9b94d672f68d7
Pierre-Francois Houessou <81161636+hpfpv@users.noreply.github.com>
2022-05-01 19:51:50 +0100

    Update download.py

--- backend/instaScraper/modules/download.py
+++ backend/instaScraper/modules/download.py
@@ -1,6 +1,7 @@
 import requests
 import urllib.request
 import os
+import json
 from functools import lru_cache
 from pymediainfo import MediaInfo
 import logging
@@ -16,23 +17,39 @@ bucketurl = f'https://{webbucket}.s3.amazonaws.com/'
 
 # @lru_cache(maxsize=128)
 def profile_picture(url, filename):
+
+    log = {}
+    log["function"] = "profile_picture"
+
     dir = 'data/profile'
     filekey = f"{dir}/{filename}.jpg"
     file = f"{filename}.jpg"
+
     try:
         s3.head_object(Bucket=webbucket, Key=filekey)
     except botocore.exceptions.ClientError as e:
         if int(e.response['Error']['Code']) == 404:
             # The object does not exist.
-            logger.info('Downloading profile picture')
+            log["status"] = "Downloading profile picture"
+            logger.info(json.dumps(log))
             profile_response = requests.get(url, stream=True).raw
             try: 
                 s3.upload_fileobj(profile_response, webbucket, filekey)
+                log["status"] = "completed"
+                logger.info(json.dumps(log))
             except botocore.exceptions.ClientError as e:
-                logger.info(e.response['Error'])
+                log["status"] = f"error: {e.response['Error']}"
+                logger.info(json.dumps(log))
+    else:
+        log["status"] = "completed"
+        logger.info(json.dumps(log))
 
 # @lru_cache(maxsize=128)
 def story_media(video, display, is_video, filename):
+
+    log = {}
+    log["function"] = "story_media"
+
     dir = 'data/media'
     video_filekey = f"{dir}/{filename}.mp4"
     display_filekey = f"{dir}/{filename}.jpg"
@@ -45,29 +62,44 @@ def story_media(video, display, is_video, filename):
         except botocore.exceptions.ClientError as e:
             if int(e.response['Error']['Code']) == 404:
             # The object does not exist.
-                logger.info('Downloading story media')
+                log["status"] = "Downloading story media"
+                logger.info(json.dumps(log))
                 video_response = requests.get(video, stream=True).raw
                 try:
                     s3.upload_fileobj(video_response, webbucket, video_filekey)
+                    log["status"] = "completed"
+                    logger.info(json.dumps(log))
                 except botocore.exceptions.ClientError as e:
-                    logger.info(e.response['Error'])
+                    log["status"] = f"error: {e.response['Error']}"
+                    logger.info(json.dumps(log))
                 
                 display_response = requests.get(display, stream=True).raw
                 try:
                     s3.upload_fileobj(display_response, webbucket, display_filekey)
                 except botocore.exceptions.ClientError as e:
-                    logger.info(e.response['Error'])
+                    log["status"] = f"error: {e.response['Error']}"
+                    logger.info(json.dumps(log))
+        else:
+            log["status"] = "completed"
+            logger.info(json.dumps(log))
     else:
         try:
             s3.head_object(Bucket=webbucket, Key=display_filekey)
         except botocore.exceptions.ClientError as e:
             if int(e.response['Error']['Code']) == 404:
             # The object does not exist.
-                logger.info('Downloading story media')
+                log["status"] = "Downloading story media"
+                logger.info(json.dumps(log))
                 display_response = requests.get(display, stream=True).raw
                 try:
                     s3.upload_fileobj(display_response, webbucket, display_filekey)
+                    log["status"] = "completed"
+                    logger.info(json.dumps(log))
                 except botocore.exceptions.ClientError as e:
-                    logger.info(e.response['Error'])
+                    log["status"] = f"error: {e.response['Error']}"
+                    logger.info(json.dumps(log))
+        else:
+            log["status"] = "completed"
+            logger.info(json.dumps(log))
                     
             
